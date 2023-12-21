3473e7602b8642642fd267e38b2ad45667c414d0
homewok <popvlad090@gmail.com>
2023-12-21 14:58:01 +0200

    ex1

---
+++ src/BirthYearCalculator.java
@@ -0,0 +1,21 @@
+public class BirthYearCalculator {
+    private static final int CURRENT_YEAR = 2023;
+
+    public static int calculateBirthYear(int age) throws InvalidAgeException{
+        if(age <= 0){
+            throw new InvalidAgeException("Invalid age");
+        }
+        int birthYear = CURRENT_YEAR - age;
+        return birthYear;
+    }
+
+    public static void main(String[] args) {
+        try{
+            int age = 46;
+            int birthYear = calculateBirthYear(age);
+            System.out.println(birthYear);
+        } catch (InvalidAgeException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
